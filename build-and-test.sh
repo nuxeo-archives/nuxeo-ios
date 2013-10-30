e9a4730d71914474772d9ef7df36db41abf90f64
Arnaud Kervern <akervern@nuxeo.com>
2013-10-30 18:49:51 +0100

    NXP-13058: Fix installAfterRestart file

--- build-and-test.sh
+++ build-and-test.sh
@@ -18,7 +18,9 @@ $NX_INT/download.sh
 # Make wizard done.
 echo "nuxeo.wizard.done=true" >> tomcat/bin/nuxeo.conf
 # deploy nuxeo-rest-api
-echo "init\ninstall nuxeo-rest-api" >> tomcat/nxserver/data/installAfterRestart.log
+echo "init" >> tomcat/nxserver/data/installAfterRestart.log
+echo "install nuxeo-rest-api" >> tomcat/nxserver/data/installAfterRestart.log
+exit 1
 start_server $HERE/tomcat 127.0.0.1
 
 # Prepare CocoaPod workspace
